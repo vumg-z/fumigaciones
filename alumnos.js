43ecdd5e6089626653876a527d507707abec263d
vumg <ab17100222@gmail.com>
2023-03-25 15:09:34 -0600

    AJUSTES

--- alumnos.js
+++ alumnos.js
@@ -4,12 +4,13 @@ export const alumnos = [
         "texto_informativo": "Humanista, Comunista, trabajando siempre por economías solidarias. Fundador de La Roja, cervecería para la inclusión social, y colaborador en otros proyectos de impacto social. Estudia filosofía budista como herramienta de desarrollo y es profesor de inglés. Ciudadano del mundo Colombo-Irlandés. Twitter: @larojacerveza"
     },
     {
-        "nombre": "Polítologo",
-        "texto_informativo": "Especialista en Justicia, Víctimas y Construcción de Paz, maestrante en Políticas Públicas en la Universidad Nacional de Colombia. Joven investigador con interés en la Construcción de Paz, Conflicto Armado, Políticas Públicas y Estudios de Juventud. Experiencia en producción de conocimiento tanto en espacios académicos como autónomos de las organizaciones populares. Ha participado en planificación estratégica desde organizaciones de economía social y solidaria, además de diversos espacios de educación popular y memoria histórica. Su última publicación fue: 'Producción social de la ruta de reincorporación de las FARC-EP, subsunción sistémica y reproducción de la conflictividad armada' (2022)."
+        "nombre": "Carlos Amador Martinez Ventura",
+        "texto_informativo": "Estudiante en la Orden Hsu Yun del Budismo Chan/Zen y Licenciado con el título de Médico cirujano por la Universidad Veracruzana."
     },
     {
         "nombre": "Juan Camilo",
-        "texto_informativo": "Estudiante en la Orden Hsu Yun del Budismo Chan/Zen y Licenciado con el título de Médico cirujano por la Universidad Veracruzana."
+        "texto_informativo": "Politólogo, Especialista en Justicia, Víctimas y Construcción de Paz y Maestrante en Políticas Públicas, todo ello en la Universidad Nacional de Colombia. Joven investigador con interés en  la Construcción de Paz, Conflicto Armado, Políticas Públicas y Estudios de Juventud. Experiencia en producción de conocimiento tanto en espacios académicos, como autónomos de las organizaciones populares; planificación estratégica desde organizaciones de economía social y solidaria, además de diversos espacios de educación popular y memoria histórica. La última publicación fue: 'Producción social de la ruta de reincorporación de las FARC-EP, subsunción sistémica y reproducción de la conflictividad armada' (2022)"
+        
     }
 ];
 
