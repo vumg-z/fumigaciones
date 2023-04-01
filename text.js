2938a61e8d6726e00cd3ce4cc991e5f215d2dc7c
vumg <ab17100222@gmail.com>
2023-03-31 19:40:30 -0600

    names updated

--- text.js
+++ text.js
@@ -4,7 +4,7 @@ export const personas = [
         "texto_informativo": "Maestro en la orden Hsu Yun del Budismo Chan/Zen desde octubre 25 del 2013\nMaestro ordenado en la tradición Zen con el nombre de Ten Kei Ken Shi desde enero 10 del 2011 \nBodhisattva en la escuela karma kagyu de budismo tibetano bajo el nombre de Karma Sonam desde julio 16 del 2008\nCofundador y responsable de la Fundación Orden Hsu Yun del Budismo Chan/Zen Latinoamérica (OHYL) \nCofundador de la Fundación Mente en Orden\nFilosofo Marxista en formación.\nBackground académico https://www.linkedin.com/in/zheng-xian-shakya-3274831bb/",
     },
     {
-        "nombre": "Wu Gong Shakya/Sunim Manhwa",
+        "nombre": "Rev. Wu Gong Shakya/Sunim Manhwa",
         "texto_informativo": "Monje zen de la orden de Seung Sahn Sunim en Corea del Sur desde 2010. Ordenado como monje en entrenamiento en el templo de Chikyisa en Kimcheon en la provincia de Chung Chon Buk-do en 2011, y como bhikku en el mismo templo en la primavera de 2018. Ha pasado retiros de meditación en varios templos famosos de Corea del Sur. Aprendió a cocinar comida tradicional de templo coreana de la monja Jeong Kwan Sunim, llegando a ser el jefe de cocina en varias ocasiones. También ha sido maestro de canto y ha realizado ceremonias en algunos templos. Actualmente en México, busca compartir lo aprendido en sus años de monje zen con otras personas."
     },
     {
@@ -12,35 +12,35 @@ export const personas = [
         "texto_informativo": "Originario de Córdoba, Veracruz, México. Ocupación: Enfermero con 12 años de servicio. Después de un evento doloroso, comenzó su búsqueda espiritual a través de diversas religiones y estudios de religión comparada y antropología religiosa. Participante en grupos de apoyo religioso y trabajo con bandas callejeras. Decidió iniciar un proceso de investigación personal hasta que encontró la Orden Hsu Yun de manera fortuita."
     },
     {
-        "nombre": "Enrique Prieto de la Rosa (Huì Han Shakya)",
-        "texto_informativo": "Formado en pintura y dibujo publicitario, grabado artístico y mosaico. Estudios en historia del cine y lenguaje audiovisual. Profesionalmente enfocado en el campo de la salud mental. Practicante de artes marciales chinas, budismo zen soto en el Dojo zen de Sevilla y budismo tibetano Kagyu durante más de treinta años."
+        "nombre": "Rev. Huì Han Shakya",
+        "texto_informativo": "Enrique Prieto de la Rosa, Formado en pintura y dibujo publicitario, grabado artístico y mosaico. Estudios en historia del cine y lenguaje audiovisual. Profesionalmente enfocado en el campo de la salud mental. Practicante de artes marciales chinas, budismo zen soto en el Dojo zen de Sevilla y budismo tibetano Kagyu durante más de treinta años."
     },
     {
-        "nombre": "Maestra del Dharma Rev. Wu Mìng Shakya ( OHYL ) Daniela Londoño Serna",
-        "texto_informativo": "Graduada de la Universidad Nueva Granada en Biología Aplicada - Biología/Ciencias biológicas, general. Bilingüe ingles/español. Docente de bachillerato en Gimnasio Campestre Los Cerezos https://www.linkedin.com/in/daniela-londoño-serna-20aab6129"
+        "nombre": "Rev. Wu Mìng Shakya (OHYL)",
+        "texto_informativo": " Daniela Londoño Serna, Graduada de la Universidad Nueva Granada en Biología Aplicada - Biología/Ciencias biológicas, general. Bilingüe ingles/español. Docente de bachillerato en Gimnasio Campestre Los Cerezos https://www.linkedin.com/in/daniela-londoño-serna-20aab6129"
     },
     {
-        "nombre": "Wu Chan Shakya",
+        "nombre": "Rev. Wu Chan Shakya",
         "texto_informativo": "Arquitecto y residente en la ciudad de San Salvador. Estudiante del Dharma desde 2009, ordenado en la Orden Hsu Yun en 2018 (Rinzai). Aficionado al montañismo, amante de la música clásica, ópera, jazz, blues y heavy metal.",
     },
     {
-        "nombre": "Maestro del Dharma en la Orden Hsu Yun del Budismo Chan/Zen",
-        "texto_informativo": "Ordenación obtenida el día 28 de noviembre del 2020. Formación en psicología budista, filosofía estoica y humanismo así como en derechos humanos. Difusor de la sicóloga budista aplicada en el día a día en mi comunidad, fiel creyente en el humanismo y aplicación continua del pensamiento crítico para el mejoramiento del presente aquí y ahora. De ocupación técnico laboratorista en Lázaro Cárdenas Michoacán México."
+        "nombre": "Rev. Wú Dao Shakya.",
+        "texto_informativo": "Mauricio Moreno Méndez. (Colombiano/ 14 de Noviembre de 1979/ Santiago DE Cali)\nMaestro de la Orden HSU YUN (Enero 15 de 2023)\n• Shifu SHI HENG HUÍ. Maestro de Kung Fu del Templo Shaolin (Ordenado en Buenos Aires, Argentina el 13 de Agosto se 2005)\nFaja negra 5° Tuan de Shaolin Wu Shu (INTERNATIONAL KUNG FU COUNCIL) BENGALA, INDIA).\nFundador de la escuela SHAOLIN DA MO WU COLOMBIA.\nMAESTRO DE QI GONG & TAI JI & SANDA.\n\nMaestro de HAPKIDO HAEMUKWAN\nCinturón negro 4° DAN de HAPKIDO (OTORGADO POR EL GRAND MASTER RICHARD HACKWORTH – THE KOREAN HAPKIDO FEDERATION & KOREAN MARTIAL ARTS INSTRUCTORS ASSOCIATION)\nLicenciado en literatura en la Universidad Del Valle. (Con énfasis profesional en guión y cine). Actor de cine (COMIN SUN FILMS).\nMaestro de educación primaria con 22 de experiencia, terapeuta graduado en Buenos Aires Argentina.\nSecretario General de la federación de Kung Fu Wu Shu de las Américas (Con cede en U.S.A.)"
     },
     {
-        "nombre": "Dr. Raul Cordova Rev. Wu Fa Shakya",
-        "texto_informativo": "Después de un camino tortuoso tratando de dilucidar la esencia de las enseñanzas de Buddha, conocí al maestro Zheng Xian Shakya y comencé en este proyecto del cual al día de hoy sigo aprendiendo. Poder estar en el sitio correcto y en la línea de pensamiento en la cual siempre quise estar no tiene comparación. Pero lo más importante de todo es poder estar aterrizado en la propuesta del Buddha. Me sigo considerando un eterno aprendiz. https://www.linkedin.com/in/dr-raul-c-acosta/"
+        "nombre": "Rev. Wu Fa Shakya",
+        "texto_informativo": "Dr. Raul Cordova, Después de un camino tortuoso tratando de dilucidar la esencia de las enseñanzas de Buddha, conocí al maestro Zheng Xian Shakya y comencé en este proyecto del cual al día de hoy sigo aprendiendo. Poder estar en el sitio correcto y en la línea de pensamiento en la cual siempre quise estar no tiene comparación. Pero lo más importante de todo es poder estar aterrizado en la propuesta del Buddha. Me sigo considerando un eterno aprendiz. https://www.linkedin.com/in/dr-raul-c-acosta/"
     },
     {
         "nombre": "Rev. Huì Zhì Shakya (Nelson Solorio Talavera)",
         "texto_informativo": "Nació en Chihuahua, México el 26 de febrero de 1983, es licenciado en literatura (Universidad Autónoma de Chihuahua), maestro en antropología social (Escuela de Antropología e Historia del Norte de México) y doctor en educación, artes y humanidades (UACH). El 6 de enero de 2021 se ordenó como maestro del Dharma, bajo la tutela de la maestra Wu Liang y el maestro Zheng Xian Shakya."
     },
     {
-        "nombre": "Wú Yi Shakya. Camila Andrea Barrera Arévalo",
-        "texto_informativo": "Maestra del Dharma en la Fundacion Orden Hsu Yun del Budismo Chan/Zen Latinoamerica. Actualmente cursa septimo semestre de sociologia en la Universidad Externado de Colombia. Universidad que ostenta el titulo de ser la primera universidad laica del pais."
+        "nombre": "Rev. Wú Yi Shakya. ",
+        "texto_informativo": "Camila Andrea Barrera Arévalo, maestra del Dharma en la Fundacion Orden Hsu Yun del Budismo Chan/Zen Latinoamerica. Actualmente cursa septimo semestre de sociologia en la Universidad Externado de Colombia. Universidad que ostenta el titulo de ser la primera universidad laica del pais."
     },
     {
-        "nombre": "Maestro",
+        "nombre": "Rev. Huì Dao Shakya",
         "texto_informativo": "Maestro del Dharma en la Orden Hsu Yun del Budismo Chan/Zen ordenación obtenida el día 28 de noviembre del 2020. Formación en psicología budista, filosofía estoica y humanismo así como en derechos humanos. Difusor de la sicóloga budista aplicada en el día a día en mi comunidad, fiel creyente en el humanismo y aplicación continua del pensamiento crítico para el mejoramiento del presente aquí y ahora. De ocupación técnico laboratorista en Lázaro Cárdenas Michoacán México."
     }
 ];
